de153eb16dd3d4333827451ed89deb03ec475b66
David Buchanan <d@vidbuchanan.co.uk>
2024-02-16 07:01:21 +0000

    restructuring

--- src/atmst/__init__.py
+++ src/atmst/__init__.py
@@ -2,7 +2,7 @@ from .blockstore import BlockStore, MemoryBlockStore
 from .blockstore.car_reader import ReadOnlyCARBlockStore
 from .mst.node_walker import NodeWalker
 from .mst.node_store import NodeStore
-from .mst.wrangler import NodeWrangler
+from .mst.node_wrangler import NodeWrangler
 from .mst.diff import mst_diff, very_slow_mst_diff, record_diff
 
 __all__ = [
