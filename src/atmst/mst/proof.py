fdd56e9242d96b4838aadcc1154b4d799ec22d54
David Buchanan <d@vidbuchanan.co.uk>
2024-12-21 11:20:45 +0000

    inclusion/exclusion proof builder helper

--- src/atmst/mst/proof.py
+++ src/atmst/mst/proof.py
@@ -6,7 +6,12 @@ from .node import MSTNode
 from .node_store import NodeStore
 from .node_walker import NodeWalker
 
-class InvalidProof(Exception):
+# validating a proof failed
+class InvalidProof(ValueError):
+	pass
+
+# constructing a proof failed
+class ProofError(ValueError):
 	pass
 
 # works for both inclusion and exclusion proofs
@@ -15,6 +20,18 @@ def find_rpath_and_build_proof(ns: NodeStore, root_cid: CID, rpath: str) -> Tupl
 	value = walker.find_rpath(rpath) # returns None if not found
 	proof = {frame.node.cid for frame in walker.stack}
 	return value, proof
+
+def build_exclusion_proof(ns: NodeStore, root_cid: CID, rpath: str) -> Set[CID]:
+	value, proof = find_rpath_and_build_proof(ns, root_cid, rpath)
+	if value is not None:
+		raise ProofError("can't build exclusion proof for a record that exists!")
+	return proof
+
+def build_inclusion_proof(ns: NodeStore, root_cid: CID, rpath: str) -> Set[CID]:
+	value, proof = find_rpath_and_build_proof(ns, root_cid, rpath)
+	if value is None:
+		raise ProofError("can't build inclusion proof for a record that doesn't exist!")
+	return proof
 
 def verify_inclusion(ns: NodeStore, root_cid: CID, rpath: str) -> None:
 	walker = NodeWalker(ns, root_cid)
